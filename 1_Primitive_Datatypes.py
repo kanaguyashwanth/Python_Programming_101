eedfeaeb20f61c05c52fdb41330a158f18dc7f35
Yashwanth K <kanaguyashwanth@gmail.com>
2023-08-07 21:17:52 +0530

    Add files via upload

---
+++ 1_Primitive_Datatypes.py
@@ -0,0 +1,25 @@
+# PRIMITIVE DATATYPES:
+'''
+- STRINGS:
+    Why they are called strings? Ans: String of characters 
+    "Hello"[0] = "H" ---> SUBSCRIPT
+    "123" + "345" = "123345"
+    
+- INTEGERS:
+    What are Integers? Ans: Whole Numbers (Numbers WITHOUT Decimal places)
+    123 + 345 = 468
+    LARGE INTEGERS:(342,654,896) 342_654_896 = 342654896
+        
+- FLOAT:
+    What are floating point numbers? Ans: Numbers with DECIMAL places
+    3.14159
+    The decimal could float around any point that's why its called floating point
+    Example: it could be 3.14159 or 314.159 or 31415.9
+    
+- BOOLEAN:
+    True / False
+'''
+
+
+
+
