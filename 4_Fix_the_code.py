4c34f128f357f9936997ee231742aedb09458cc6
Yashwanth K <kanaguyashwanth@gmail.com>
2023-08-18 12:42:56 +0530

    4_Fix_the_code.py

--- 4_Fix_the_code.py
+++ 4_Fix_the_code.py
@@ -8,7 +8,7 @@ print(("New lines can be created with a backslash and n.")
 '''
 
 # CODE:
-print("Day 1 - String Manipulation")						# FIX: missing " inside the print statement
+print("Day 1 - String Manipulation")						        # FIX: missing " inside the print statement
 print("String Concatenation is done with '+' sign.")		# FIX: single-quotes inside double-quotes or vice-versa
-print('e.g. print("Hello " + "world")')						# FIX: remove extra indent space at the beginning of print statement
+print('e.g. print("Hello " + "world")')						      # FIX: remove extra indent space at the beginning of print statement
 print("New lines can be created with a backslash and n.")   # FIX: ((, only single (
