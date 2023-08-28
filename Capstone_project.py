dba51de51de0e86caf3e0037a0ca269e763e8183
Yashwanth K <kanaguyashwanth@gmail.com>
2023-08-28 21:09:08 +0530

    Add files via upload

---
+++ Capstone_project.py
@@ -0,0 +1,52 @@
+# Day - 11: CAPSTONE PROJECT [BLACKJACK / 21]
+
+'''
+RULES:
+ - Whoever manages to have cards which sum close to 21, wins.
+ - If it crosses 21, then you lose immediately. It is also known as Bust.
+ - cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+ - Cards are infinite in the DECK, once removed, there are still more cards.
+ - Cards are not removed from the Deck when drawn.
+ - EACH card has EQUAL chances of occurring.
+ - The ACE can count as 1 or 11.
+ - There are no JOKERS.
+
+ - Cards with 2 to 10, count as their face value
+ - J, Q, K - Count as 10 each
+ - ACE - Can either count as 1 or as a 11
+ - We DEAL till we get close to 21 or we can close the deal and the dealer reveals their cards.
+ - If the dealer and you both end up in same score, then it is a DRAW!
+ - If the dealer has 21 and we have less, then we LOSE!
+ - If we have 21 and the dealer has less, then we WIN!
+
+
+Your cards: [9, 10], current score: 19
+Computer's first card: 8
+Type 'y' to get another card, type 'n' to pass: n
+Your final hand: [9, 10]
+Computer's final hand: [8, 10]
+You Win!
+
+Do you want to play a game of Blackjack? Type 'y' or 'n': y
+
+Your cards: [5, 10], current score: 15
+Computer's first card: 10
+Type 'y' to get another card, type 'n' to pass: y
+Your final hand: [5, 10, 10], final score: 25
+Computer's final hand: [10, 10], final score: 20
+You went over. You lose.
+
+
+'''
+
+
+def blackjack ():
+    
+
+
+
+
+
+
+
+blackjack()
