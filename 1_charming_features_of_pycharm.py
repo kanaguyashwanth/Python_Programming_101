c083a22b844ab8fc65884935265a84d9d9b598f3
Yashwanth K <kanaguyashwanth@gmail.com>
2023-09-04 23:07:17 +0530

    Add files via upload

---
+++ 1_charming_features_of_pycharm.py
@@ -0,0 +1,14 @@
+'''
+CHARMING FEATURES OF PYCHARM:
+
+Text Editor vs. IDE:
+- Spell Check
+- Applies rule and guidances like Indentation
+- Style Guide (Yellow-squiggly lines)
+- Local History (Reverting to Snapshots)
+- View Structure (all variables, all functions) [Structure-pane]
+- Refactor Rename (BETTER than find and replace! Suppose there's a print statment with a text which you don't wanna change, the factor will not change it.)
+    - Right-click name of function or variable
+    - Change the name (changes it everywhere!)
+    - Click refactor ---> Click Do Refactor
+'''
